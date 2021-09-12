c0f97d3f4e0914a60c8229b8f63eba8eeae32e85
Francesco Verdoja <francesco.verdoja@aalto.fi>
2021-09-12 08:25:30 +0300

    Improved generator tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,58 @@
 import pytest
 import yaml
 from boostertutor.generator import MtgPackGenerator
+from boostertutor.models.mtg_card import MtgCard
+from boostertutor.models.mtg_pack import MtgPack
 
 
 @pytest.fixture(scope="session")
 def generator():
     with open("config.yaml") as file:
-        config = yaml.load(file, Loader=yaml.FullLoader)
-    jmp = config["jmp_decklists_path"] if "jmp_decklists_path" in config else None
-    return MtgPackGenerator(path_to_mtgjson=config["mtgjson_path"],
+        conf = yaml.load(file, Loader=yaml.FullLoader)
+    jmp = conf["jmp_decklists_path"] if "jmp_decklists_path" in conf else None
+    return MtgPackGenerator(path_to_mtgjson=conf["mtgjson_path"],
                             path_to_jmp=jmp, jmp_arena=True)
+
+
+@pytest.fixture
+def unbalanced_pack(generator):
+    m20 = generator.data.sets["M20"].cards_by_ascii_name
+    content = {
+        "basicOrCommonLand": {
+            "cards": [MtgCard(m20['bloodfell caves'])],
+            "balance": False
+        },
+        "nonlandCommon": {
+            "cards": [
+                MtgCard(m20['thicket crasher']),
+                MtgCard(m20['destructive digger']),
+                MtgCard(m20['raise the alarm']),
+                MtgCard(m20['angelic gift']),
+                MtgCard(m20['ripscale predator']),
+                MtgCard(m20['inspiring captain']),
+                MtgCard(m20['bladebrand']),
+                MtgCard(m20['gorging vulture']),
+                MtgCard(m20['griffin protector']),
+                MtgCard(m20['act of treason'])
+            ],
+            "backups": [
+                MtgCard(m20['fortress crab'])
+            ],
+            "balance": True
+        },
+        "rareMythic": {
+            "cards": [MtgCard(m20['thunderkin awakener'])],
+            "balance": False
+        },
+        "uncommon": {
+            "cards": [
+                MtgCard(m20['blightbeetle']),
+                MtgCard(m20['overcome']),
+                MtgCard(m20['herald of the sun'])
+            ],
+            "balance": False
+        },
+
+    }
+    p = MtgPack(content)
+    return p
--- tests/generator_test.py
+++ tests/generator_test.py
@@ -1,9 +1,15 @@
 from collections import Counter
 
+import pytest
+
 
 def test_pack_generation(generator):
     p = generator.get_pack("m21")
     assert len(p.cards) == 15
+    assert p.is_balanced(rebalance=False)
+
+    with pytest.raises(AssertionError):
+        p = generator.get_pack("non existing set")
 
     p_list = generator.get_pack("znr", n=6)
     assert len(p_list) == 6
@@ -16,13 +22,20 @@ def test_random_pack_generation(generator):
     assert len(p_list) == 4
     assert set(set_list) == set([pack.set.code for pack in p_list])
 
-    p_list = generator.get_random_pack(sets=set_list, n=6, replace=True)
-    assert len(p_list) == 6
+    with pytest.raises(AssertionError):
+        p_list = generator.get_random_pack(sets=set_list, n=5)
+
+    p_list = generator.get_random_pack(sets=set_list, n=5, replace=True)
+    assert len(p_list) == 5
     count_sets = Counter([pack.set.code for pack in p_list])
     assert any([c > 1 for c in count_sets.values()])
 
 
-def test_rebalancing(generator):
-    p = generator.get_pack("DOM")
-    assert len(p.cards) == 15
-    assert p.is_balanced(rebalance=False)
+def test_balancing(unbalanced_pack):
+    assert not unbalanced_pack.is_balanced()
+    unbalanced_pack.is_balanced(rebalance=True)
+    assert unbalanced_pack.is_balanced()
+
+    cards = [c.card.name for c in unbalanced_pack.cards]
+    assert 'Griffin Protector' not in cards
+    assert 'Fortress Crab' in cards
