d7ab5ac3e545edf7a984eb253bf4ec3f1587d3d1
Francesco Verdoja <francesco.verdoja@aalto.fi>
2021-09-13 09:40:59 +0300

    More testing

--- tests/generator_test.py
+++ tests/generator_test.py
@@ -3,7 +3,7 @@ from collections import Counter
 import pytest
 
 
-def test_pack_generation(generator):
+def test_pack(generator):
     p = generator.get_pack("m21")
     assert len(p.cards) == 15
     assert p.is_balanced(rebalance=False)
@@ -16,7 +16,7 @@ def test_pack_generation(generator):
     assert all([pack.set.code == "ZNR" for pack in p_list])
 
 
-def test_random_pack_generation(generator):
+def test_random_pack(generator):
     set_list = ["MB1", "APC", "MIR", "AKR"]
     p_list = generator.get_random_pack(sets=set_list, n=4)
     assert len(p_list) == 4
@@ -29,6 +29,15 @@ def test_random_pack_generation(generator):
     assert len(p_list) == 5
     count_sets = Counter([pack.set.code for pack in p_list])
     assert any([c > 1 for c in count_sets.values()])
+
+
+def test_jumpstart(generator):
+    p = generator.get_random_jmp_deck()
+    assert len(p.cards) == 20
+
+    for d in generator.data.sets["JMP"].decks:
+        for c in d["mainBoard"]:
+            assert c.name != "Path to Exile"
 
 
 def test_balancing(unbalanced_pack):
