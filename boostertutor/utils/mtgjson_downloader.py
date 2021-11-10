e5c4239504a2fa18e007cfa342dc39be90add071
Francesco Verdoja <francesco.verdoja@aalto.fi>
2021-11-10 16:28:25 +0200

    added parameter parsing to mtgjson downloader

--- boostertutor/utils/mtgjson_downloader.py
+++ boostertutor/utils/mtgjson_downloader.py
@@ -1,3 +1,4 @@
+import argparse
 import shutil
 from pathlib import Path
 from zipfile import ZipFile
@@ -34,42 +35,66 @@ def download_jmp_decks(
     backup: bool = False,
 ):
     temp_path = Path(temp_dir) / "temp_decks"
-    temp_path.mkdir(exist_ok=True)
     fn = "AllDeckFiles.zip"
-    download_file(url, temp_path / fn)
-    with ZipFile(temp_path / fn, "r") as zipObj:
-        # Extract all the contents of zip file in current directory
-        zipObj.extractall(temp_path)
+    try:
+        temp_path.mkdir(exist_ok=True)
+        download_file(url, temp_path / fn)
+        with ZipFile(temp_path / fn, "r") as zipObj:
+            # Extract all the contents of zip file in current directory
+            zipObj.extractall(temp_path)
 
-    path = Path(dir)
-    if backup and path.exists():
-        path.rename(Path(path.parent, "JMP_last"))
-    deck_path = path / "decks"
-    deck_path.mkdir()
-    for fp in temp_path.glob("*JMP.json"):
-        fp.rename(Path(deck_path, fp.name))
+        path = Path(dir)
+        if backup and path.exists():
+            shutil.rmtree(path.parent / "JMP_last")
+            path.rename(path.parent / "JMP_last")
 
-    shutil.rmtree(temp_path)
+        if not path.exists():
+            path.mkdir()
+
+        deck_path = path / "decks"
+        deck_path.mkdir(exist_ok=True)
+        for fp in temp_path.glob("*JMP.json"):
+            fp.rename(Path(deck_path, fp.name))
+    finally:
+        shutil.rmtree(temp_path)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Downloads MTGJSON data.")
+    parser.add_argument(
+        "--jmp", help="download JMP decks", action="store_true"
+    )
+    parser.add_argument(
+        "--jmp-backup", help="backup old JMP decks", action="store_true"
+    )
+    args = parser.parse_args()
+
     print("Reading config...")
 
     with open("config.yaml") as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
-    download_jmp = "jmp_decklists_path" in config
+    download_jmp = args.jmp and "jmp_decklists_path" in config
 
     print(f"MTGjson data will be downloaded in: {config['mtgjson_path']}")
     if download_jmp:
         print(
             f"JMP decks will be downloaded in: {config['jmp_decklists_path']}"
         )
+        if args.jmp_backup:
+            jmp_backup_path = (
+                Path(config["jmp_decklists_path"]).parent / "JMP_last/"
+            )
+            print(
+                f"Old JMP decks will be moved to: {jmp_backup_path.as_posix()}"
+            )
     else:
         print("JMP deck will not be downloaded")
 
-    print("Beginning MTGjson data download...")
+    print("\nBeginning MTGjson data download...")
     download_mtgjson_data(file=config["mtgjson_path"])
 
     if download_jmp:
         print("Beginning JMP decks download...")
-        download_jmp_decks(dir=config["jmp_decklists_path"])
+        download_jmp_decks(
+            dir=config["jmp_decklists_path"], backup=args.jmp_backup
+        )
