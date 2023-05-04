1480595b003507902e17b2ea3f4ee3c15087136d
Francesco Verdoja <f.verdoja@gmail.com>
2023-05-04 12:20:19 +0000

    better sorting of foils

--- boostertutor/models/mtg_card.py
+++ boostertutor/models/mtg_card.py
@@ -115,7 +115,7 @@ class MtgCard:
         )
         return (
             is_common_land + is_basic_land,
-            self.foil,
+            not self.foil,
             r.index(self.card.rarity),
         )
 
